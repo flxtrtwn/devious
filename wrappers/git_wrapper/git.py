079a215447ef6cbda4eb14be648308a0c0485958
flxtrtwn <mail@felixtrautwein.de>
2023-06-16 19:26:40 +0200

    merge pull by default

--- wrappers/git_wrapper/git.py
+++ wrappers/git_wrapper/git.py
@@ -50,8 +50,10 @@ def remote_rename(old_name: str, new_name: str) -> None:
     subprocess.run(remote_rename_cmd)
 
 
-def pull(remote: str = "origin", branch: str = "main") -> None:
-    pull_cmd = ["git", "pull", remote, branch]
+def pull(remote: str = "origin", branch: str = "main", rebase:bool=False) -> None:
+    pull_cmd = ["git", "pull"]
+    pull_cmd.extend([f"--rebase={str(rebase).lower()}"])
+    pull_cmd.extend([remote, branch])
     subprocess.run(pull_cmd)
 
 def set_default_remote_for_branch(remote_name:str="origin", local_branch_name:str="main", remote_branch_name:str="main") -> None:
