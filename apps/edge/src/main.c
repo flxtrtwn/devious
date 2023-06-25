295795ab497b380cea0a5af209046f5fe3af99b1
flxtrtwn <mail@felixtrautwein.de>
2023-06-25 23:25:25 +0200

    Add blink example

--- apps/edge/src/main.c
+++ apps/edge/src/main.c
@@ -1,1 +1,12 @@
-void app_main() {}
+#include <Arduino.h>
+
+void setup(){
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+}
