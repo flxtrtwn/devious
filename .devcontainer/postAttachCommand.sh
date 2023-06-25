4bcb9d5fc995f3e7fc048bc354c102ddd50a15c5
flxtrtwn <mail@felixtrautwein.de>
2023-06-25 23:41:45 +0200

    Remove udev restart

--- .devcontainer/postAttachCommand.sh
+++ .devcontainer/postAttachCommand.sh
@@ -9,7 +9,5 @@ git lfs install
 git lfs track "*.png"
 git lfs track "*.jpg"
 
-service udev restart
-
 find /home/$USER/.platformio/packages/contrib-piohome/ -maxdepth 1 -name "main*" -exec sed -i 's#"\\\\":"/"#"/":"/"#g' {} +
 sudo chmod a+rw /dev/ttyACM0
