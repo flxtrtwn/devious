ead577b67cd7b01cae11cdbfdabd2209a789fe99
flxtrtwn <mail@felixtrautwein.de>
2023-06-26 22:05:44 +0200

    Not fail for device not present

--- .devcontainer/postAttachCommand.sh
+++ .devcontainer/postAttachCommand.sh
@@ -9,5 +9,5 @@ git lfs install
 git lfs track "*.png"
 git lfs track "*.jpg"
 
-find /home/$USER/.platformio/packages/contrib-piohome/ -maxdepth 1 -name "main*" -exec sed -i 's#"\\\\":"/"#"/":"/"#g' {} +
-sudo chmod a+rw /dev/ttyACM0
+find /home/$USER/.platformio/packages/contrib-piohome/ -maxdepth 1 -name "main*" -exec sed -i 's#"\\\\":"/"#"/":"/"#g' {} + || true
+sudo chmod a+rw /dev/ttyACM0 || true
