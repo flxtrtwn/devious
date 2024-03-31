360655b0640d64e2f645d0a328b1ea0cd83d76ea
Felix Trautwein <mail@felixtrautwein.de>
2024-04-01 00:54:35 +0200

    feat: Refactor android build

--- .devcontainer/postAttachCommand.sh
+++ .devcontainer/postAttachCommand.sh
@@ -18,9 +18,9 @@ cp ~/.Xauthority "$DOCKER_XAUTHORITY"
 
 #Change permissions of /dev/kvm for Android Emulator
 echo "$(whoami)" | sudo -S chmod 777 /dev/kvm > /dev/null 2>&1
-export PATH=$PATH:/studio-data/platform-tools/ # export will not work
+# export PATH=$PATH:/studio-data/platform-tools/ # export will not work
 # Ensure the Android directory exists and has the correct permissions
-if [ ! -d "/studio-data/Android" ]; then
-  mkdir -p /studio-data/Android
-fi
-sudo chown -R "${USER}":"${USER}" /studio-data/Android
+# if [ ! -d "/studio-data/Android" ]; then
+#   mkdir -p /studio-data/Android
+# fi
+# sudo chown -R "${USER}":"${USER}" /studio-data/Android
