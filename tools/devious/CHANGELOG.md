3beb785ba330d976df465aad33c3f6144f45c9e8
Felix Trautwein <50261759+flxtrtwn@users.noreply.github.com>
2024-11-09 14:16:30 +0100

    Fix/django project setup (#18)
    * feat: Create sqlite db on creation of django app
    
    * chore: Bump version
    
    * fix: Copy debug settings
    
    * feat: Improved microservice main
    
    * docs: Add secret mount hint

---
+++ tools/devious/CHANGELOG.md
@@ -0,0 +1,9 @@
+# 0.7.0 (2024-11-9)
+
+## Features
+
+-   Improvements for microservices main module
+
+## Bugfixes
+
+-   Sqlite DB is created on `create` for Django projects
--- tools/devious/src/devious/targets/django_app/django_app.py
+++ tools/devious/src/devious/targets/django_app/django_app.py
@@ -51,9 +51,13 @@ class DjangoApp(Target):
         target_src_dir.mkdir(parents=True)
         subprocess.run(["django-admin", "startproject", target_name, str(target_src_dir)], check=True)
         subprocess.run(["chmod", "+x", str((target_src_dir / "manage.py"))], check=True)
+        shutil.copy(APP_CONFIG_DIR / "debug_settings.py", target_src_dir / target_name)
         requirements_file = target_dir / "requirements.txt"
         requirements_file.touch()
         docker_compose_file = target_dir / "docker-compose.yaml"
+        sqlite_db = target_dir / "test" / "db.sqlite3"
+        sqlite_db.parent.mkdir()
+        sqlite_db.touch()
         ruamel.yaml.YAML().dump(
             {
                 "services": {
