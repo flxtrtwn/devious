c73912c50a2df77bcd2d8260992474cff0d0b5ba
Felix Trautwein <50261759+flxtrtwn@users.noreply.github.com>
2024-12-16 22:56:23 +0100

    Feat/flutter (#17)
    * refactor: Move user creation to base stage
    
    * feat: Add flutter stage
    
    * refactor: Quote variable expansions
    
    * feat: Newer versions of android studio and flutter
    
    * docs: Add todo
    
    * feat: Add flutter vscode extensions
    
    * feat: Add openjdk-17
    
    * feat: Refactor android build
    
    * feat: Install android-sdk and flutter in user dir
    
    * feat: Install android studio
    
    * fix: Add missing packages
    
    * format: whitespace

--- .devcontainer/postAttachCommand.sh
+++ .devcontainer/postAttachCommand.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-source ${HOME}/.bashrc
+source "${HOME}"/.bashrc
 
 set -euo pipefail
 shopt -s nullglob globstar
 
 poetry config virtualenvs.in-project true
 poetry install --no-cache
-source $(poetry env info --path)/bin/activate
+source "$(poetry env info --path)"/bin/activate
 
 pre-commit install >/dev/null 2>&1
 git lfs install >/dev/null 2>&1
@@ -15,3 +15,14 @@ git lfs track "*.jpg" >/dev/null 2>&1
 
 DOCKER_XAUTHORITY=${HOME}/.Xauthority.docker
 cp ~/.Xauthority "$DOCKER_XAUTHORITY"
+
+#Change permissions of /dev/kvm for Android Emulator
+echo "$(whoami)" | sudo -S chmod 777 /dev/kvm > /dev/null 2>&1
+# export PATH=$PATH:/studio-data/platform-tools/ # export will not work
+# Ensure the Android directory exists and has the correct permissions
+# if [ ! -d "/studio-data/Android" ]; then
+#   mkdir -p /studio-data/Android
+# fi
+# sudo chown -R "${USER}":"${USER}" /studio-data/Android
+
+yes | flutter doctor --android-licenses || test $? -eq 141
