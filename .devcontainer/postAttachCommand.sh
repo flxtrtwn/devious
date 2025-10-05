153c5cf8306432a5cfe04437aecfaf59dee99793
Felix Trautwein <50261759+flxtrtwn@users.noreply.github.com>
2025-10-05 16:16:38 +0200

    chore: Deactivate xauth

--- .devcontainer/postAttachCommand.sh
+++ .devcontainer/postAttachCommand.sh
@@ -11,5 +11,5 @@ git lfs install >/dev/null 2>&1
 git lfs track "*.png" >/dev/null 2>&1
 git lfs track "*.jpg" >/dev/null 2>&1
 
-DOCKER_XAUTHORITY=${HOME}/.Xauthority.docker
-cp ~/.Xauthority "$DOCKER_XAUTHORITY"
+# DOCKER_XAUTHORITY=${HOME}/.Xauthority.docker
+# cp ~/.Xauthority "$DOCKER_XAUTHORITY"
